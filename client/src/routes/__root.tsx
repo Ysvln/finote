f6b1602b9556836f29640694e31444882a08119a
Ysvln <ysvln0912@gmail.com>
2025-09-01 17:38:35 +0900

    feat: 사용자용 Sidebar(UserSidebar) 추가

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,35 +1,16 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { AppLayout } from "./layouts/AppLayout";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
-    <>
-      <div className="p-2 flex gap-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>{' '}
-        <Link
-          to="/about"
-          activeProps={{
-            className: 'font-bold',
-          }}
-        >
-          About
-        </Link>
-      </div>
-      <hr />
+    <AppLayout>
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
-    </>
-  )
+    </AppLayout>
+  );
 }
