7d94bd0dc77fa5ec0a2bbe7646eca16be76b3e26
Ysvln <ysvln0912@gmail.com>
2025-09-03 13:37:36 +0900

    temp: sidebar trigger 주석처리

--- client/src/routes/layouts/AppLayout.tsx
+++ client/src/routes/layouts/AppLayout.tsx
@@ -1,19 +1,18 @@
 import { SidebarProvider, SidebarTrigger } from "@/shared/ui/sidebar";
-import { UserSidebar } from "@/widgets/sidebar/UserSidebar";
+import { UserSidebar } from "@/widgets/sidebar/userSidebar";
 import { ReactNode } from "react";
 
 interface AppLayoutProps {
   children?: ReactNode;
 }
 // TODO : 로그인 및 회원가입 구현 후 권한별 sidebar 처리
-
 export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex min-h-screen">
       <SidebarProvider>
         <UserSidebar />
-        <main className="flex-1 p-4">
-          <SidebarTrigger className="mb-4" />
+        <main className="flex-1 py-12 p-6">
+          {/* <SidebarTrigger className="mb-4" /> */}
           {children}
         </main>
       </SidebarProvider>
